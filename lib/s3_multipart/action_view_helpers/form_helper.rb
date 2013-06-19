2deeab9440c5af35740d09c863d89392960f2e33
Junya Wako <j.wako@j-q.co.jp>
2013-06-19 12:36:28 +0900

    Remove multiple options

--- lib/s3_multipart/action_view_helpers/form_helper.rb
+++ lib/s3_multipart/action_view_helpers/form_helper.rb
@@ -3,7 +3,7 @@ module S3Multipart
     module FormHelper
       def multipart_uploader_form(options = {})
         uploader_digest = S3Multipart::Uploader.serialize(options[:uploader])
-        html = file_field_tag options[:input_name], :accept => options[:types].join(','), :multiple => 'multiple', :data => {:uploader => uploader_digest}
+        html = file_field_tag options[:input_name], :accept => options[:types].join(','), :data => {:uploader => uploader_digest}
         html << options[:html].html_safe
         html << submit_tag(options[:button_text], {:type => "button", :class => options[:button_class]})
         # html << button_tag(:class => options[:button_class]) do
