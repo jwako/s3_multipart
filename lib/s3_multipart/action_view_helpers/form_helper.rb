9ae30c7b153d7ca665cd82b6bedbf3909d383f29
Junya Wako <j.wako@j-q.co.jp>
2013-07-04 14:52:39 +0900

    Adds js

--- lib/s3_multipart/action_view_helpers/form_helper.rb
+++ lib/s3_multipart/action_view_helpers/form_helper.rb
@@ -5,7 +5,7 @@ module S3Multipart
         uploader_digest = S3Multipart::Uploader.serialize(options[:uploader])
         html = file_field_tag options[:input_name], :accept => options[:types].join(','), :data => {:uploader => uploader_digest}
         html << options[:html].html_safe
-        html << submit_tag(options[:button_text], {:type => "button", :class => options[:button_class]})
+        html << submit_tag(options[:button_text], {:type => "button", :class => options[:button_class], :onclick => "jmedia.uploadCheck();" })
         # html << button_tag(:class => options[:button_class]) do
         #   content_tag(:span, options[:button_text])
         # end
